687367d5208f651b582ba1a82c7329868577992d
Pierre Saugues <psaugues@MBP-de-Pierre-2.lan>
2024-10-16 10:51:27 +0200

    font

--- wp-content/themes/ymmersion/header.php
+++ wp-content/themes/ymmersion/header.php
@@ -5,6 +5,9 @@
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
     <title>Page <?php echo get_the_title(); ?> des Ecuries du Pouey</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
     <meta property="og:image" content="<?php echo get_template_directory_uri() . '/assets/images/png/logo-social.png'; ?>">
 </head>
