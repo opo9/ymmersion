5e275d0eda6e423a592d9c2cb9c1a8179f42521d
Tenma <tenma@osxt.com>
2024-10-16 11:53:52 +0200

    add services

--- wp-content/themes/ymmersion/front-page.php
+++ wp-content/themes/ymmersion/front-page.php
@@ -2,12 +2,22 @@
 /* Template Name: Page d'accueil */
 get_header();
 get_template_part('parts/header');
+
+$args = array(
+    'post_type' => 'services',
+    'posts_per_page' => -1,
+    'orderby' => 'date',
+    'post_status' => 'publish',
+    'order' => 'ASC',
+);
+
+$services = new WP_Query($args);
 ?>
 
-<?php if (have_posts()): ?>
-    <?php while (have_posts()): ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : ?>
         <?php the_post(); ?>
-        <main class="home bg-background">
+        <main class="home bg-background ">
             <!-- Bannière -->
             <section class="relative h-[524px]">
                 <?php $banniere = get_field('banniere'); ?>
@@ -25,71 +35,79 @@ get_template_part('parts/header');
             <!-- Nos Valeurs -->
             <section class="p-24 space-y-16 w-2/3 m-auto">
                 <?php $valeurs = get_field('valeurs'); ?>
-                <div class="wysiwyg--custom"><?= $valeurs["titre"] ?></div>
+                
+             <div class="wysiwyg--custom"><?= $valeurs["titre"]?></div>
                 <div class="flex justify-between ">
-                    <?php foreach ($valeurs["liste"] as $valeur): ?>
-                        <div class="flex flex-col items-center">
-                            <img class="w-20" src="<?= $valeur["image"]["url"] ?>" alt="<?= $valeur["image"]["alt"] ?>">
-                            <p><?= $valeur["titre"] ?></p>
-                        </div>
-                    <?php endforeach; ?>
+                <?php foreach($valeurs["liste"] as $valeur): ?>
+                    <div class="flex flex-col items-center">
+                        <img class="w-20" src="<?= $valeur["image"]["url"] ?>" alt="<?= $valeur["image"]["alt"] ?>">
+                        <p><?= $valeur["titre"]?></p>
+                  
+                    </div>
+                <?php endforeach; ?>
                 </div>
             </section>
             <!--  Chevaux à vendre -->
             <section class="bg-background ">
                 <?php $elements = get_field('chevaux_a_vendre'); ?>
-
+                
                 <!-- <?= var_dump($chevaux_a_vendre) ?> -->
-                <div class="flex h-[400px]">
+                 <div class="flex">
                     <?php $droite = $elements["droite"] ?>
-                    <img class="w-1/2" src="<?= $elements["gauche"]["image"]["url"] ?>"
-                        alt="<?= $elements["gauche"]["image"]["alt"] ?>">
-                    <div class="bg-primary w-1/2 text-white p-20 ">
-                        <h2><?= $droite["titre"] ?></h2>
-                        <p class="text-left mt-5"><?= $droite["description"] ?></p>
+
+                     <img class="w-1/2 brightness-75" src="<?= $elements["gauche"]["image"]["url"] ?>" alt="<?= $elements["gauche"]["image"]["alt"] ?>">
+                     <div class="bg-primary w-1/2 text-white p-20 ">
+                        <h2 ><?= $droite["titre"]?></h2>
+                        <p class="text-left mt-5"><?= $droite["description"]?></p>
                         <div class="mt-20">
                             <!-- Bouton -->
-                            <a class="bg-button p-btn rounded-2xl text-black" href=<?= $droite["bouton"]["url"] ?>><?= $droite["bouton"]["title"] ?></a>
-                            <div>
-                            </div>
-                        </div>
-
-                    </div>
+                        <a class="bg-button p-btn rounded-2xl text-black" href=<?=$droite["bouton"]["url"]?> ><?= $droite["bouton"]["title"]?></a>
+                        <div>
+            </div>
+                     </div>
+                     
+                 </div>
             </section>
-            <!-- Services -->
-            <?php $services = get_field('services'); ?>
-            <section class="p-20 space-y-8">
-                <h2 class="text-center"><?= $services["titre"] ?></h2>
+          <!-- Services -->
+          <section class="p-20 space-y-8">
+            <h2 class="text-center"></h2>
+     
 
-                <div class="text-white flex justify-between gap-10 w-max m-auto">
-
-                    <!-- Gauche -->
-                    <a href=<?= $services["liste"]["gauche"]["url"] ?> alt=<?= $services["liste"]["gauche"]["url"] ?>>
-                        <div class="bg-secondary  h-20 w-48 p-4">
-                            <?= $services["liste"]["gauche"]["title"] ?>
+            <?php if ($services->have_posts()) : ?>
+    <div class="text-white flex justify-between gap-10 w-max m-auto">
+        <?php $count = 0; ?>
+        <?php while ($services->have_posts()) : $services->the_post(); ?>
+            <?php if ($count >= 3) break; ?>
+            
+                <?php if (have_rows('service')) : ?>
+                    <?php while (have_rows('service')) : the_row(); ?>
+                        <a href=<?= the_sub_field("url")?>  alt=<?= the_sub_field("url")?>>
+                        <div class="flex flex-col justify-between h-24 w-48 p-4 <?php
+                             if ($count === 0) {
+                                echo 'bg-secondary';
+                            } elseif ($count === 1) {
+                                echo 'bg-primary';
+                            } elseif ($count === 2) {
+                                echo 'bg-tertiary';
+                            };
+                        ?>">
+                        <?= the_sub_field('titre');?>
+                        <img class="ml-auto w-5  h-auto" src="/wp-content/themes/ymmersion/assets/images/svg/arrow.svg" alt="arrow" loading="lazy">
                         </div>
-                    </a>
+                </a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
 
-                    <!-- Milieu -->
-                    <a href=<?= $services["liste"]["milieu"]["url"] ?> alt=<?= $services["liste"]["milieu"]["url"] ?>>
-                        <div class="bg-primary  h-20 w-48 p-4">
-                            <?= $services["liste"]["milieu"]["title"] ?>
-                        </div>
-                    </a>
+            <?php $count++; ?>
+        <?php endwhile; ?>
+    </div>
 
-                    <!-- Droite -->
-                    <a href=<?= $services["liste"]["droite"]["url"] ?> alt=<?= $services["liste"]["droite"]["url"] ?>>
-                        <div class="bg-tertiary  h-20 w-48 p-4">
-                            <?= $services["liste"]["droite"]["title"] ?>
-                        </div>
-                    </a>
-                </div>
-
-            </section>
-
-
-        </main>
-    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+<?php else : ?>
+    <p>Aucun service trouvé.</p>
 <?php endif; ?>
+        </section>
+    </main>
+<?php endwhile; endif; ?>
 
 <?php get_template_part('parts/footer'); ?>
