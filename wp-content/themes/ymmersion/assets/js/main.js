d7f9147bdcb742f621e9b35d7b599f4460c3f54e
Pierre Saugues <psaugues@MBP-de-Pierre-2.lan>
2024-10-16 13:03:31 +0200

    update

--- wp-content/themes/ymmersion/assets/js/main.js
+++ wp-content/themes/ymmersion/assets/js/main.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-})
-;
+    const observer = new MutationObserver(function (mutationsList, observer) {
+        mutationsList.forEach(function (mutation) {
+            if (mutation.addedNodes.length > 0) {
+                document.querySelectorAll('.sk_branding').forEach(function (element) {
+                    element.style.setProperty('display', 'none', 'important'); // Applique display none avec !important
+                });
+            }
+        });
+    });
+
+    // Observe the entire document for changes
+    observer.observe(document.body, {childList: true, subtree: true});
+
+    const swiper = new Swiper('#slider-home', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 4000,
+        },
+        slidesPerView: '3',
+        centerInsufficientSlides: true,
+        allowTouchMove: true,
+        simulateTouch: true,
+        pagination: {
+            el: '.swiper-pagination', // Cible l'élément de pagination dans le HTML
+            clickable: true, // Rendre les dots cliquables
+        },
+    });
+
+    $('#slider-home').mouseenter(function () {
+        swiper.autoplay.stop();
+    });
+
+    $('#slider-home').mouseleave(function () {
+        swiper.autoplay.start();
+    });
+
+});
--- wp-content/themes/ymmersion/front-page.php
+++ wp-content/themes/ymmersion/front-page.php
@@ -12,6 +12,16 @@ $args = array(
 );
 
 $servicesPost = new WP_Query($args);
+
+$args = array(
+    'post_type' => 'avis',
+    'posts_per_page' => -1,
+    'orderby' => 'date',
+    'post_status' => 'publish',
+    'order' => 'ASC',
+);
+
+$avis = new WP_Query($args);
 ?>
 
 <?php if (have_posts()) : ?>
@@ -23,101 +33,142 @@ $servicesPost = new WP_Query($args);
                 <?php $banniere = get_field('banniere'); ?>
                 <!-- Background Image (SVG) -->
                 <img class="absolute inset-0 w-full h-full bg-no-repeat bg-cover hero-bg-position-custom"
-                    style="background-image: url('<?= $banniere["background_image"]["url"] ?>" alt="Bannière SVG">
+                     style="background-image: url('<?= $banniere["background_image"]["url"] ?>" alt="Bannière SVG">
                 <div class="z-10  inset-0 absolute bg-primary opacity-80"></div>
                 <div class="relative flex flex-col justify-center h-full z-20 p-24 space-y-20">
                     <h1 class="text-white text-4xl font-bold"><?= $banniere["titre"] ?></h1>
                     <div>
-                        <a class=" bg-button p-btn rounded-2xl " href=<?= $banniere["bouton"]["url"] ?>><?= $banniere["bouton"]["title"] ?></a>
+                        <a class=" bg-button p-btn rounded-2xl "
+                           href=<?= $banniere["bouton"]["url"] ?>><?= $banniere["bouton"]["title"] ?></a>
                     </div>
                 </div>
             </section>
             <!-- Nos Valeurs -->
             <section class="p-24 space-y-16 w-2/3 m-auto">
                 <?php $valeurs = get_field('valeurs'); ?>
-                
-             <div class="wysiwyg--custom"><?= $valeurs["titre"]?></div>
+
+                <div class="wysiwyg--custom"><?= $valeurs["titre"] ?></div>
                 <div class="flex justify-between ">
-                <?php foreach($valeurs["liste"] as $valeur): ?>
-                    <div class="flex flex-col items-center">
-                        <img class="w-20" src="<?= $valeur["image"]["url"] ?>" alt="<?= $valeur["image"]["alt"] ?>">
-                        <p><?= $valeur["titre"]?></p>
-                  
-                    </div>
-                <?php endforeach; ?>
+                    <?php foreach ($valeurs["liste"] as $valeur): ?>
+                        <div class="flex flex-col items-center">
+                            <img class="w-20" src="<?= $valeur["image"]["url"] ?>" alt="<?= $valeur["image"]["alt"] ?>">
+                            <p><?= $valeur["titre"] ?></p>
+
+                        </div>
+                    <?php endforeach; ?>
                 </div>
             </section>
             <!--  Chevaux à vendre -->
             <section class="bg-background ">
                 <?php $elements = get_field('chevaux_a_vendre'); ?>
-                
-                <!-- <?= var_dump($chevaux_a_vendre) ?> -->
-                 <div class="flex">
+
+                <div class="flex">
                     <?php $droite = $elements["droite"] ?>
 
-                     <img class="w-1/2 brightness-75" src="<?= $elements["gauche"]["image"]["url"] ?>" alt="<?= $elements["gauche"]["image"]["alt"] ?>">
-                     <div class="bg-primary w-1/2 text-white p-20 ">
-                        <h2 ><?= $droite["titre"]?></h2>
-                        <p class="text-left mt-5"><?= $droite["description"]?></p>
+                    <img class="w-1/2 brightness-75" src="<?= $elements["gauche"]["image"]["url"] ?>"
+                         alt="<?= $elements["gauche"]["image"]["alt"] ?>">
+                    <div class="bg-primary w-1/2 text-white p-20 ">
+                        <h2><?= $droite["titre"] ?></h2>
+                        <p class="text-left mt-5"><?= $droite["description"] ?></p>
                         <div class="mt-20">
                             <!-- Bouton -->
-                        <a class="bg-button p-btn rounded-2xl text-black" href=<?=$droite["bouton"]["url"]?> ><?= $droite["bouton"]["title"]?></a>
-                        <div>
-            </div>
-                     </div>
-                     
-                 </div>
-            </section>
-          <!-- Services -->
-          <section class="p-20 space-y-8">
-            <h2 class="text-center"></h2>
-     
-
-            <?php if ($servicesPost->have_posts()) : ?>
-    <div class="flex flex-col text-black space-y-5 w-[600px] m-auto">
-    <?php $services = get_field('services');?>
-
-        <h2 class="text-center text-3xl font-bold "><?= $services["titre"]?></h2>
-    <div class="text-white flex justify-between gap-10 ">
-        <?php $count = 0; ?>
-        <?php while ($servicesPost->have_posts()) : $servicesPost->the_post(); ?>
-            <?php if ($count >= 3) break; ?>
-            
-                <?php if (have_rows('service')) : ?>
-                    <?php while (have_rows('service')) : the_row(); ?>
-                        <a class="w-1/3" href=<?= the_sub_field("url")?>  alt=<?= the_sub_field("url")?>>
-                        <div class="flex flex-col justify-between h-24 p-4 <?php
-                             if ($count === 0) {
-                                echo 'bg-secondary';
-                            } elseif ($count === 1) {
-                                echo 'bg-primary';
-                            } elseif ($count === 2) {
-                                echo 'bg-tertiary';
-                            };
-                        ?>">
-                        <?= the_sub_field('titre');?>
-                        <img class="ml-auto w-5 h-auto" src="/wp-content/themes/ymmersion/assets/images/svg/arrow.svg" alt="arrow" loading="lazy">
+                            <a class="bg-button p-btn rounded-2xl text-black"
+                               href=<?= $droite["bouton"]["url"] ?>><?= $droite["bouton"]["title"] ?></a>
+                            <div>
+                            </div>
                         </div>
-                </a>
-                    <?php endwhile; ?>
+
+                    </div>
+            </section>
+            <!-- Services -->
+            <section class="p-20 space-y-8">
+                <h2 class="text-center"></h2>
+                <?php if ($servicesPost->have_posts()) : ?>
+                    <div class="flex flex-col text-black space-y-5 w-[600px] m-auto">
+                        <?php $services = get_field('services'); ?>
+
+                        <h2 class="text-center text-3xl font-bold "><?= $services["titre"] ?></h2>
+                        <div class="text-white flex justify-between gap-10 ">
+                            <?php $count = 0; ?>
+                            <?php while ($servicesPost->have_posts()) : $servicesPost->the_post(); ?>
+                                <?php if ($count >= 3) break; ?>
+
+                                <?php if (have_rows('service')) : ?>
+                                    <?php while (have_rows('service')) : the_row(); ?>
+                                        <a class="w-1/3"
+                                           href=<?= the_sub_field("url") ?>  alt=<?= the_sub_field("url") ?>>
+                                            <div class="flex flex-col justify-between h-24 p-4 <?php
+                                            if ($count === 0) {
+                                                echo 'bg-secondary';
+                                            } elseif ($count === 1) {
+                                                echo 'bg-primary';
+                                            } elseif ($count === 2) {
+                                                echo 'bg-tertiary';
+                                            };
+                                            ?>">
+                                                <?= the_sub_field('titre'); ?>
+                                                <img class="ml-auto w-5 h-auto"
+                                                     src="/wp-content/themes/ymmersion/assets/images/svg/arrow.svg"
+                                                     alt="arrow" loading="lazy">
+                                            </div>
+                                        </a>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+
+                                <?php $count++; ?>
+                            <?php endwhile; ?>
+                        </div>
+                        <!-- Lien services -->
+                        <a class="ml-auto flex items-center underline cursor-pointer"><?= $services["lien"]["title"] ?>
+
+                            >
+
+                        </a>
+                    </div>
+                    <?php wp_reset_postdata(); ?>
+                <?php else : ?>
+                    <p>Aucun service trouvé.</p>
                 <?php endif; ?>
+            </section>
+            <!-- Avis -->
+            <section class="avis">
+                <div class="title">
+                    <?php echo get_field("temoignages")["titre"]; ?>
+                </div>
 
-            <?php $count++; ?>
-        <?php endwhile; ?>
-    </div>
-    <!-- Lien services -->
-    <a class="ml-auto flex items-center underline cursor-pointer"><?= $services["lien"]["title"]?>
-  
-   >
+                <div class="swiper" id="slider-home">
+                    <div class="swiper-wrapper">
+                        <?php if ($avis->have_posts()) : ?>
+                            <?php while ($avis->have_posts()) : ?>
+                                <?php $avis->the_post(); ?>
+                                <?php $slide = get_field("avis"); ?>
+                                <div class="swiper-slide">
+                                    <div class="container">
+                                        <div class="logo">
+                                            “
+                                        </div>
 
-</a>
-    </div>
-    <?php wp_reset_postdata(); ?>
-<?php else : ?>
-    <p>Aucun service trouvé.</p>
-<?php endif; ?>
-        </section>
-    </main>
-<?php endwhile; endif; ?>
+                                        <div class="titre">
+                                            <?php echo $slide["titre"]; ?>
+                                        </div>
+
+                                        <div class="desc">
+                                            <?php echo $slide["description"]; ?>
+                                        </div>
+
+                                        <div class="author">
+                                            <?php echo $slide["auteur"]; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="swiper-pagination"></div>
+                </div>
+            </section>
+        </main>
+    <?php endwhile; endif; ?>
 
 <?php get_template_part('parts/footer'); ?>
